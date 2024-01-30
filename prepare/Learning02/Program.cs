a5ea9289608498463b62eca14d87d8b4a77ad9f5
SameepSalekar <sameepsalekar749@gmail.com>
2024-01-29 19:28:11 -0600

    Complete Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+     {
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2009";
+        job1._endYear = "2013";
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = "2013";
+        job2._endYear = "2018";
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+     }
 }
