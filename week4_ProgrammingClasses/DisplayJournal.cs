d41e3b744e408bf211ce0c4fff6cb1b4a626874f
SameepSalekar <sameepsalekar749@gmail.com>
2024-02-02 21:05:35 -0600

    Trial Program

--- week4_ProgrammingClasses/DisplayJournal.cs
+++ week4_ProgrammingClasses/DisplayJournal.cs
@@ -2,12 +2,29 @@ using System;
 
 public class DisplayJournal
 {
-    public string _enterDate;
+    public DateTime _dateTareek = DateTime.Now ;
 
-    public string _promptResponse
-    
+    List<string> _questions = new List<string>();
+    _questions.Add("Who was the most interesting person you interacted today?");
+    _questions.Add("What was the best part of your day?");
+    _questions.Add("How did you see the hand of the Lord in your life today?");
+    _questions.Add("Write about the strongest emotion you felt today?");
+    _questions.Add("If you had one thing, you could do today, what would that be?");
+
     public void AskQuestion()
     {
-        
+
+        for each (string _promptRespone in _questions)
+        {
+           Console.WriteLine(_questions);
+           _promptResponse = Console.ReadLine(_questions);
+
+           
+        }
+    }
+
+    public void DisplayPromptResponses()
+    {
+
     }
 }
--- week4_ProgrammingClasses/Program.cs
+++ week4_ProgrammingClasses/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[]args)
     {
+        DisplayJournal journalone = new DisplayJournal();
+        journalone._questions.Add("Who was the most interesting person you interacted today?");
+        journalone._questions.Add("What was the best part of your day?");
+        journalone._questions.Add("How did you see the hand of the Lord in your life today?");
+        journalone._questions.Add("Write about the strongest emotion you felt today?");
+        journalone._questions.Add("If you had one thing, you could do today, what would that be?");
+
+
+        SavedJournal journalek = new SavedJournal();
+
+        _entryResponse = -1
+
+        while (_entryResponse > 5)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("What would you like to do today?")
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Exit");
+            _entryResponse = (int.Parse(Console.ReadLine));
+
+            if (_entryResponse == 1 )
+            {
+                journalone.AskQuestion();
+            }
+            else if (_entryResponse == 2)
+            {
+                journalone.DisplayPromptResponses();
+            }
+            else if (_entryResponse == 3)
+            {
+                journalek.SaveFileName();
+            }
+
+            else if (_entryResponse == 4)
+            {
+                journalek.LoadTheFile();
+            }
+
+            else (_entryResponse == 5)
+            {
+                Console.WriteLine("Thanks for using the app.");
+                Console.WriteLine("We would love to see you back.")
+                Console.WriteLine("Hope you have great upcoming days :)");
+            }
+
+
+
+
+        }
+
+
+
+        DisplayJournal journalone = new DisplayJournal();
         
+
+        SavedJournal journalek = new SavedJournal();
+
+
     }
 }
