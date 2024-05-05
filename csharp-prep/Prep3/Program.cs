84b0b843e5d1085d517d0ccd23fdb96836423339
SameepSalekar <sameepsalekar749@gmail.com>
2024-05-05 15:21:45 -0500

    Prep-3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        int number = int.Parse(Console.ReadLine());
+
+
+        Random randomGenerator = new Random();
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");                
+            }                        
+        }
     }
 }
